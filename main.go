037806a0ad7ed8953db25367907f32871364656a
hiepndd <nguyendangdaihiep@gmail.com>
2019-02-20 18:05:50 +0700

    add channel in order to implement expired

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -26,11 +27,13 @@ func parseLines(lines [][]string) []problem {
 
 func exit(msg string) {
 	fmt.Println(msg)
+
 	os.Exit(1)
 }
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
@@ -44,18 +47,27 @@ func main() {
 		exit("Failed to parse the provided CSV file.")
 	}
 	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
+problemloop:
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
-			fmt.Println("Correct")
-		} else {
-			fmt.Println("Incorrect")
-			return
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
+
 	}
 
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
